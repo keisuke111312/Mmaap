28c2a26031edd2157c044b356286cdb6fd3a437e
Keisuke Karishuku <kkarishuku@gmail.com>
2025-07-02 20:29:03 +0800

    Add custom Nix environment for Railway (PHP 8.2)

---
+++ railway.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.php82
+    pkgs.composer
+    pkgs.nodejs_20
+    pkgs.mysql
+    pkgs.git
+  ];
+}
